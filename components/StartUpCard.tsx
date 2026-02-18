0db69fd3cdbb3011ab17ba0b07e319db1b38c1ef
Hermi-git <andargiehermela1@gmail.com>
2026-02-18 12:52:51 +0300

    feat: integrate Sanity CMS with Next.js

--- components/StartUpCard.tsx
+++ components/StartUpCard.tsx
@@ -7,7 +7,7 @@ import { Button } from "./ui/button";
 const StartUpCard = ({post}) => {
     const {_createdAt, views, _id, author: {_id:authorId, name}, category, title, description, image } = post
   return (
-    <li className="bg-white border-[5px] border-black py-6 px-5 rounded-[22px] shadow-200 hover:border-primary transition-all duration-500 hover:shadow-300 hover:bg-primary-100;
+    <li className="bg-white border-[5px] border-black py-6 px-5 rounded-[22px] shadow-200 hover:border-primary transition-all duration-500 hover:shadow-300 hover:bg-primary-100
    group">
         <div className="flex justify-between items-center">
             <p className="font-medium text-[16px] bg-primary-100 px-4 py-2 rounded-full group-hover:bg-white-100">
